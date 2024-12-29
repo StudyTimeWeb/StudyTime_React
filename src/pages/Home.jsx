e57406c4d01becd0fa589325009eac83a634953b
chaechaen <leece144@gmail.com>
2024-12-29 11:04:33 +0900

    feat: initialize settings

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Home = () => {
+    return (
+        <Container>
+        </Container>
+    )
+}
+
+export default Home;
+
+const Container = styled.div`
+`;
